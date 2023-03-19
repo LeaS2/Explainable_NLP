cc358a92eff18318cb57c8b5c01a52d4697a83a9
B43M <maxbleick@live.de>
2023-03-19 12:23:57 +0100

    new training run with less steps and smaller save steps

--- Vitaminc/scripts/run_fact_verification.sh
+++ Vitaminc/scripts/run_fact_verification.sh
@@ -2,24 +2,26 @@
 
 set -ex
 export PYTHONPATH="${PYTHONPATH}:${PWD}/Explainable_NLP/"
+pip install -r requirements.txt
 python scripts/fact_verification.py \
   --model_name_or_path tals/albert-base-vitaminc-fever \
   --tasks_names feverous \
   --data_dir data \
-  --do_test \
   --max_seq_length 256 \
   --per_device_train_batch_size 8 \
   --per_device_eval_batch_size 128 \
   --learning_rate 2e-5 \
-  --max_steps 50000 \
-  --save_step 10000 \
+  --max_steps 20000 \
+  --save_step 4000 \
   --overwrite_cache \
-  --output_dir results/feverous_2 \
+  --do_test \
+  --test_on_best_ckpt\
+  --output_dir results/feverous_less_steps \
   --eval_all_checkpoints \
-  #--test_on_best_ckpt \
-  --do_train
+  --do_train\
+  --do_eval\
   "$@"
-
+#--do_train
   #--fp16 \
   #--test_tasks vitc_real vitc_synthetic \
   #--do_predict \
